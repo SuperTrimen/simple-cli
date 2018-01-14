79112591a8b3d62650d30e6c5d8e480ac5cc1b9e
罗维辉 <lwh1@strongsoft.net>
2018-01-14 21:06:29 +0800

    修改 模板调整

--- template/src/main.js
+++ template/src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 {{#if_eq cliType 'plugin'}}import VuePlugin from './lib/'
 {{/if_eq}}import App from './App.vue'
 {{#if_eq cliType 'plugin'}}Vue.use(VuePlugin)// 或者传入参数对象 Vue.use(VuePlugin,{})
-{{/if_eq}}let vm = new Vue({
+{{/if_eq}}
+let vm = new Vue({
   el: '#app',
   render: h => h(App)
 })
